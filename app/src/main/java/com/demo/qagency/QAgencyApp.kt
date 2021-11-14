2507805752e232e8f1a77e118f1fd9d2711c17f3
Nikola Crnogorac <crnogorac.nikola93@gmail.com>
2021-11-14 22:42:48 +0100

    added DI

---
+++ app/src/main/java/com/demo/qagency/QAgencyApp.kt
@@ -0,0 +1,17 @@
+package com.demo.qagency
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class QAgencyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG)
+        {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
