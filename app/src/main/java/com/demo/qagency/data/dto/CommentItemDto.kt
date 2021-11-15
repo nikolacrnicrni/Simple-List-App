8a6bf308eeaa516ee84bd73a17e8091f5c16b521
Nikola Crnogorac <crnogorac.nikola93@gmail.com>
2021-11-15 19:11:21 +0100

    added dto

---
+++ app/src/main/java/com/demo/qagency/data/dto/CommentItemDto.kt
@@ -0,0 +1,17 @@
+package com.demo.qagency.data.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class CommentItemDto(
+    @SerializedName("body")
+    val body: String,
+    @SerializedName("email")
+    val email: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("postId")
+    val postId: Int
+)
